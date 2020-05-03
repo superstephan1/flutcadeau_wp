fbfa51d6d9df36336137c45565f3db3e3ea43e48
Stephan <stephan.kop@outlook.com>
2020-05-03 17:26:33 +0200

    small setup of template

---
+++ wp-content/themes/flutcadeau/footer.php
@@ -0,0 +1,13 @@
+<div class="footer">
+    <div class="col-lg-3">
+        <p>Flutcadeau's voor alle gelegenheden zijn op deze affiliate website te vinden.</p>
+    </div>
+    <div class="col-lg-3">
+        <p>Flutcadeau copyright 2020</p>
+    </div>
+    <div class="col-lg-3 socials">
+        <a href="#">Twitter</a>
+        <a href="#">Pinterest</a>
+        <a href="#">Facebook</a>
+    </div>
+</div>
---
+++ wp-content/themes/flutcadeau/header.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="navbar">
+        <h1>Flutcadeau</h1>
+        <nav class="nav">
+            <a class="blog-nav-item active" href= "<?php echo get_bloginfo("url")?>/test1">Cadeautips</a>
+            <a class="blog-nav-item" href= "<?php echo get_bloginfo("url")?>/test2">5 tot 10 euro</a>
+            <a class="blog-nav-item" href= "<?php echo get_bloginfo("url")?>/test2">10 tot 15 euro</a>
+        </nav>
+    </div>
+</div>
